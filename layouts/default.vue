61555a9d20925d6a797e195f7c3d839b969b405a
Daniel Vieira <danielgsvieira@gmail.com>
2021-10-30 16:38:17 -0300

    Layout

---
+++ layouts/default.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>
+        {{ title }}
+      </h1>
+    </header>
+    <main>
+      <Nuxt />
+    </main>
+    <footer>
+      <span class="author-name">
+        Daniel Vieira
+      </span>
+      <a :href="githubLink">
+        Source (GitHub)
+      </a>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    const title = ref<string>('Order split');
+    const githubLink = ref<string>('https://github.com/danielgsvieira/order-split');
+
+    return {
+      githubLink,
+      title,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/variables';
+
+.container {
+  height: 100vh;
+  width: 100vw;
+
+  display: grid;
+  grid-template-rows: 3rem 1fr 2rem;
+
+  header {
+    width: 100%;
+    height: 3rem;
+    padding: 0 1rem;
+    border-bottom: 1px solid #EBEBEB;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  main {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer {
+    background-color: #F1F3F4;
+    padding: 0 1rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    color: $lightText;
+    font-size: 0.8rem;
+
+    a {
+      color: $lightText;
+    }
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/main.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -31,6 +32,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module',
+    ['@nuxtjs/google-fonts', { families: { Roboto: true } }],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,7 +45,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'pt_BR'
     }
   },
 
